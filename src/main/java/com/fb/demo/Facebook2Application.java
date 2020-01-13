f6ec69c2c56a0538965d96d1183f8c01c0097b02
linnaing <mdy.linnaing@gmail.com>
2020-01-13 09:17:27 +0630

    first

---
+++ src/main/java/com/fb/demo/Facebook2Application.java
@@ -0,0 +1,13 @@
+package com.fb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Facebook2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Facebook2Application.class, args);
+	}
+
+}
